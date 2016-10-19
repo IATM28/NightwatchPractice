0d2ba5b02b412ed9b4981fbe4c356cda2f6d2d40
Junior Casique <juniorcp86@gmail.com>
2016-10-19 12:48:40 -0400

    Modifications on create a new property on CE. Adding comments.

--- tests/myfirst.js
+++ tests/myfirst.js
@@ -1,51 +1,49 @@
 module.exports = {
-  'Login on CE' : function (browser) {
-   // browser
+  'Create New Property File on CE' : function (browser) {
+	  //Start Session
       browser.url('https://cedev.channelauction.com')
       browser.waitForElementVisible('body',1000)
-      //Start Session
       browser.setValue('input[name="logonName"]', 'jose')
       browser.setValue('input[name="password"]', 'jose')
       browser.click('button[data-elm-id="btnLogin"]')
       browser.waitForElementVisible('body',1000)
+	  console.log("Log in successful")
 	  
       //Navigate from home to New Property File
       browser.waitForElementVisible('a[data-elm-id="lnkCasAdministration"]',100000)
       browser.click('a[data-elm-id="lnkCasAdministration"]')
       browser.click('a[data-elm-id="lnkCasNewPropertyFile"]')
       browser.waitForElementVisible('body',1000)
-      
-	  //Load and create new properties
       browser.waitForElementVisible('select[id="client"]',100000)
-      
-	  //Product type dropdown
+	  console.log("Navigate to New Property file and wait until some elements are loaded.")
+	  
+	  //Start ---- Fill required fields for new properties
+	  //Transaction Characteristics section
 	  browser.click('select[id="productType"]')
-      browser.click('option[value="string:traditionalListing"]')
-      
-	  //Test Property dropdown
+      browser.click('option[value="string:traditionalListing"]')//Product Type
 	  browser.click('select[id="isTestProperty"]')
-	  browser.click('option[label="No"]')
+	  browser.click('option[label="No"]')//Test Property
+	  console.log("Complete Transaction Characteristics section (Product type and Test property).")
 	  
 	  //Address section
-	  //Line 1 textbox
-	  browser.setValue('input[id="line1"]','448 S 23rd St 1')	  
-	  browser.setValue('input[id="zipCode"]','94804')
-	  
+	  browser.setValue('input[id="line1"]','448 S 23rd St 1')//Line 1	  
+	  browser.setValue('input[id="zipCode"]','94804')//Zip Code
+	  browser.click('a[data-elm-id="btnMapAddress"]')
+	  	  
 	  //Seller name dropdown
-	  browserclass="form-control ng-touched ng-dirty ng-valid-parse ng-invalid ng-invalid-required"
-	  browser.click('select[id="client"]')
+	  //browserclass="form-control ng-touched ng-dirty ng-valid-parse ng-invalid ng-invalid-required"
+	  browser.click('select[id="client"]')//Seller name
 	  browser.pause(5000)
-	  //.waitForElementVisible()
-      //.click('option[value="string:583902a6-86dd-4847-a423-e1c492d3105d"]')
-	  //.pause(5000)
 	  browser.assert.containsText('select[id="client"]',"--")
 	  browser.pause(120000)
 	  browser.click('select[id="client"]')
 	  browser.keys(browser.Keys.DOWN_ARROW)
-	  browser.keys(['\uE006']) //hits the enter key.
+	  browser.keys(['\uE006'])//Hits the enter key
+	  //Finish ---- Fill required fields for new properties
 	  
-	  //.expect.element('select[id="client"]').to.have.attribute('value').wich.contains("string:583902a6-86dd-4847-a423-e1c492d3105d")
-	  .click('button[data-elm-id="btnCreate"]')
+	  //Button
+	  browser.assert.elementPresent('button[data-elm-id="btnCreate"]')
+	  browser.click('button[data-elm-id="btnCreate"]')
       //.end();
   }
 };
