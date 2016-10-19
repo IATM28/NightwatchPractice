6c4a1190f0c04128aba4ce4008013d93bf487f15
Gabriel Aguilera <dumbo1787@gmail.com>
2016-10-19 17:41:02 -0400

    added fill address(demo) and create cta

--- tests/gabo/create_file_ce.js
+++ tests/gabo/create_file_ce.js
@@ -9,7 +9,7 @@
       .waitForElementVisible('body', 100000)
   },
 
-  /*'choose create a file' : function (browser){
+  'choose create a file' : function (browser){
   	browser
       .waitForElementVisible('body', 20000)
       .waitForElementVisible('a[data-elm-id="lnkCasAdministration"]', 50000)
@@ -17,17 +17,29 @@
       .waitForElementVisible('a[data-elm-id="lnkCasNewPropertyFile"]', 20000)
       .click('a[data-elm-id="lnkCasNewPropertyFile"]')
       .waitForElementVisible('body', 20000)
-  },*/
+  },
   
   'fill transaction characteristics' : function (browser){
     browser
-      .url('https://cedev.channelauction.com/#/app/cas/property-file/new')
-      .waitForElementVisible('body', 200000)
-      .waitForElementVisible('select[data-elm-id="client"]', 500000)
-      .click('select[data-elm-id="client"]')
+      //.url('https://cedev.channelauction.com/#/app/cas/property-file/new')
+      .waitForElementVisible('select[id="client"]', 500000)
+      .click('select[id="client"]')
       .waitForElementVisible('option[value="string:c10e801f-a17c-475c-bf79-709aff55de48"]', 20000)
       .click('option[value="string:c10e801f-a17c-475c-bf79-709aff55de48"]')
       .assert.containsText('select[data-elm-id="client"]', "10195")
+      .click('select[id="productType"]')
+      .waitForElementVisible('select[id="productType"]', 200000)
+      .click('option[value="string:traditionalListing"]')    
+  },
+  /*'fill address' : function (browser){
+    browser
+      .setValue('input[data-elm-id="line1"]', 'addressline1')
+      .setValue('input[data-elm-id="zipCode"]', 'zipCode')    
+  },*/
+  'create and check' : function (browser){
+    browser
+      .click('button[data-elm-id="btnCreate"]')
+        
   }
   
 };
