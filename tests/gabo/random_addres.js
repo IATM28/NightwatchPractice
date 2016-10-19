b72d7dfb2d180ed9021bb2e6454682ee2ca01daa
Gabriel Aguilera <dumbo1787@gmail.com>
2016-10-19 18:17:24 -0400

    trying to get value from results

--- tests/gabo/random_addres.js
+++ tests/gabo/random_addres.js
@@ -3,6 +3,7 @@ module.exports = {
     browser
       .url('https://www.randomlists.com/random-addresses')
       .waitForElementVisible('ol[id="result"]', 10000)
-      .click('button[data-elm-id="btnLogin"]')
-      .waitForElementVisible('body', 10000)
+      .getText('ol[id='result']/li[1]/div]', function(randomAdress)){
+      	var adresss = randomAdress
+      }
   },
