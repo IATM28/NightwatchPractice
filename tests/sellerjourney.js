808389683b552f681c92e6d243a7929687947479
Junior Casique <juniorcp86@gmail.com>
2016-10-20 15:57:58 -0400

    Update on script for create new properties from DE. This script is ready to step 3.

--- tests/sellerjourney.js
+++ tests/sellerjourney.js
@@ -7,12 +7,11 @@ module.exports = {
 	  //browser.waitForElementPresent('a[ng-href="login?redirectUrl=/homes/"]',10000)
 	  //browser.click('a[ng-href="login?redirectUrl=/homes/"]')
       
-	  
+	  browser.maximizeWindow()
 	  browser.waitForElementVisible('body',1000)
 	  browser.waitForElementPresent('input[data-elm-id="userEmail"]',10000)
 	  browser.waitForElementPresent('input[data-elm-id="userPassword"]',10000)
 	  browser.waitForElementPresent('button[data-elm-id="btnLogin"]',10000)
-	  
 	  browser.assert.elementPresent('input[data-elm-id="userEmail"]')
 	  browser.assert.elementPresent('input[data-elm-id="userPassword"]')
 	  browser.assert.elementPresent('button[data-elm-id="btnLogin"]')
@@ -20,14 +19,17 @@ module.exports = {
 	  browser.setValue('input[data-elm-id="userEmail"]', 'jcasique+52@mahisoft.com')
       browser.setValue('input[data-elm-id="userPassword"]', 'jcasique')
       browser.click('button[data-elm-id="btnLogin"]')
-      browser.waitForElementVisible('body',1000)
+      browser.waitForElementVisible('h1[class="ng-binding"]',10000)
+	  browser.waitForElementPresent('i[class="btr bt-question-circle"]',10000)
+	  
 	  /*
 	  Aca falta colocar mouse over en la opcion List a Home.
 	  */
+	  //Start step 0
 	  browser.url('https://dedev.channelauction.com/homes/list-a-home/search')
 	  browser.waitForElementPresent('input[name="address"]',10000)
 	  browser.waitForElementPresent('button[class="btn btn-primary locate-property-btn"]',10000)
-	  browser.setValue('input[name="address"]','4801 Retriever Cir 1, Anchorage, AK 99502')
+	  browser.setValue('input[name="address"]','4814 Retriever Cir, Anchorage, AK 99502')
 	  browser.click('button[class="btn btn-primary locate-property-btn"]')
 	  browser.pause(25000)
 	  browser.keys(['\uE006'])
@@ -38,22 +40,66 @@ module.exports = {
 	  browser.waitForElementPresent('input[id="county"]',10000)
 	  browser.waitForElementPresent('input[id="city"]',10000)
 	  browser.waitForElementPresent('select[id="state"]',10000)
-	  browser.waitForElementPresent('[class="btn btn-primary col-sm-6 col-sm-offset-3"]',10000)
+	  browser.waitForElementPresent('button[class="btn btn-primary col-sm-6 col-sm-offset-3"]',10000)
 	  browser.assert.elementPresent('input[name="line1"]')
 	  browser.assert.elementPresent('input[id="line2"]')
 	  browser.assert.elementPresent('input[id="zipCode"]')
 	  browser.assert.elementPresent('input[id="county"]')
 	  browser.assert.elementPresent('input[id="city"]')
 	  browser.assert.elementPresent('select[id="state"]')
-	  browser.assert.elementPresent('[class="btn btn-primary col-sm-6 col-sm-offset-3"]')
-	  browser.click('[class="btn btn-primary col-sm-6 col-sm-offset-3"]')
+	  browser.assert.elementPresent('button[class="btn btn-primary col-sm-6 col-sm-offset-3"]')
+	  browser.click('button[class="btn btn-primary col-sm-6 col-sm-offset-3"]')
 	  //Finish step 0
-	  
-	  
-	  
-	  
-	  
+	  //start step 1
 	  browser.pause(50000)
+	  browser.waitForElementPresent('a[class="btn2 primary-btn btn btn-primary m-15"]',20000)
+	  browser.waitForElementPresent('i[class="bt-check-circle bts ng-scope"]',20000)
+	  browser.assert.elementPresent('a[class="btn2 primary-btn btn btn-primary m-15"]')
+	  browser.click('a[class="btn2 primary-btn btn btn-primary m-15"]')
+	  //Finish step 1
+	  //Start step 2
+	  browser.waitForElementPresent('div[class="text-center ng-binding"]',10000)
+	  browser.waitForElementPresent('button[data-elm-id="btnOwner"]',10000)
+	  browser.waitForElementPresent('button[data-elm-id="btnAgent"]',10000)
+	  browser.assert.elementPresent('button[data-elm-id="btnOwner"]')
+	  browser.assert.elementPresent('button[data-elm-id="btnAgent"]')
+	  browser.click('button[data-elm-id="btnOwner"]')
+	  browser.click('button[data-elm-id="btnOwner"]')
+	  browser.pause(10000)
+	  browser.waitForElementPresent('button[class="btn btn-marshmallow-rectangle checked"]',50000)
+	  browser.waitForElementPresent('button[data-elm-id="btnHaveAgentYes"]',20000)
+	  browser.waitForElementPresent('button[data-elm-id="btnHaveAgentNo"]',20000)
+	  browser.click('[data-elm-id="btnHaveAgentNo"]')
+	  browser.waitForElementPresent('button[class="btn btn-marshmallow-rectangle checked"]',5000)
+	  browser.assert.elementPresent('button[class="btn btn-marshmallow-rectangle checked"]')
+	  browser.click('button[data-elm-id="btnSave"]')
+	  //Finish step 2 as seller without agent
+	  //Start step 3
+	  browser.waitForElementPresent('select[data-elm-id="propertyType"]',20000)
+	  browser.waitForElementPresent('select[data-elm-id="beds"]',20000)
+	  browser.waitForElementPresent('select[data-elm-id="completeBathrooms"]',20000)
+	  browser.waitForElementPresent('select[data-elm-id="splitBathrooms"]',20000)
+	  browser.waitForElementPresent('input[data-elm-id="squareFootage"]',20000)
+	  browser.waitForElementPresent('input[data-elm-id="lotSizeSqft"]',20000)
+	  browser.waitForElementPresent('input[id="yearBuilt"]',20000)
+	  browser.waitForElementPresent('button[data-elm-id="btnSave"]',20000)
+	  browser.click('select[data-elm-id="propertyType"]')
+	  browser.click('option[value="string:Single Family-Detached"]')
+	  browser.click('select[data-elm-id="beds"]')
+	  browser.click('option[value="number:4"]')
+	  browser.click('select[data-elm-id="completeBathrooms"]')
+	  browser.assert.containsText('select[data-elm-id="completeBathrooms"]',"Choose Below")
+	  browser.keys(browser.Keys.DOWN_ARROW)
+	  browser.keys(browser.Keys.DOWN_ARROW)
+	  browser.keys(browser.Keys.DOWN_ARROW)
+	  browser.keys(browser.Keys.DOWN_ARROW)
+	  browser.keys(['\uE006'])
+	  browser.pause(5000)
+	  browser.click('button[data-elm-id="btnSave"]')
+	  //Finish step 3
+	  browser.pause(50000)
+	  
+	  
 	  //
 	  
 	  
