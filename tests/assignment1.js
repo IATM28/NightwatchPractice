dfe3aadfb2b5811b93665c73fda1c579f172edbf
Angel Ortega Giffard <Angel Ortega Giffard>
2016-10-27 11:23:01 -0400

    Add the already finished assignments.

--- tests/assignment1.js
+++ tests/assignment1.js
@@ -1,3 +1,7 @@
+
+// Developer: Angel Ortega - Mahisoft
+// Assignment 1: Create a new property from CS
+
 module.exports = {
   'Create a new property from CE': function (browser) {
   	var line1 = ''
@@ -8,15 +12,17 @@ module.exports = {
   	var productType = 'Traditional Listing'
 
   	browser
+
+  	// Get a random address from California
 	.url('http://www.fakeaddressgenerator.com/Random_Address/US_California')
 	.useXpath()
 	.waitForElementVisible('html/body/div[1]/div[3]/div[1]/div[1]/div/div[2]/div[7]/div[2]/strong/input', 5000)	
 	.getValue('html/body/div[1]/div[3]/div[1]/div/div[3]/div[2]/div[6]/div[2]/strong/input', function(result){ line1 = result.value })
 	.getValue('html/body/div[1]/div[3]/div[1]/div/div[3]/div[2]/div[9]/div[2]/strong/input', function(result){ zipCode = result.value })
 	
+	// CREATION OF A NEW PROPERTY GETS STARTED:
 	.perform(
 		function() {		  
-			console.log('Address: '+line1+', '+zipCode)
 			browser.useCss()
 			browser.url('https://cedev.channelauction.com')
 			browser.waitForElementVisible('body', 80000)
@@ -34,11 +40,15 @@ module.exports = {
 			browser.click('select[data-elm-id="isTestProperty"] option[label="Yes"]')
 			browser.setValue('input[data-elm-id="line1"]', line1)
 			browser.setValue('input[data-elm-id="zipCode"]', zipCode)
-			browser.pause(1000)
-			browser.click('button[data-elm-id="btnCreate"]')
-			//browser.waitForElementNotVisible('button[data-elm-id="btnCreate"]', 80000)
+			browser.pause(2000)
+			browser.submitForm('form[name="form"]')
+			browser.waitForElementVisible('div[id="fileHeader"]', 80000)
+			browser.click('a[data-elm-id="lnkCasFiles"]')
+			browser.waitForElementVisible('tbody', 80000)
+			browser.setValue('input[data-elm-id="searchAddress"]', line1)
+			browser.pause(3000)
+			browser.assert.containsText('tbody', line1)
 			}
 		)
-	}
-	  
+	}	  
 };
--- tests/assignment2.js
+++ tests/assignment2.js
@@ -1,35 +1,14 @@
-module.exports = {	
+// Developer: Angel Ortega - Mahisoft
+// Assignment 2: Create a new property from DE
+
+module.exports = {		
   'Create a new property from DE': function (browser) {
   	var userEmail = 'jcasique+52@mahisoft.com'
   	var userPassword = 'jcasique'
-  	///var zipCode = '90002'
-  	//var line1 = ''
-  	var address = '9008 Holmes Ave, Los Angeles, CA 90002'
-  	var currentNavOption = ''
+  	var address = '10336 Juniper St Los Angeles CA 90002' // without commas
+  	var photo = 'house_photo.jpg'
 
-  	/* get zipcode from CE - imcomplete
-	.url('https://cedev.channelauction.com')
-	.waitForElementVisible('body', 80000)
-	.setValue('input[ng-model="loginCtrl.logonName"]', 'jose')
-	.setValue('input[ng-model="loginCtrl.password"]', 'jose')
-	.click('button[data-elm-id="btnLogin"]')
-	.waitForElementVisible('div[class="nav-wrapper"]', 80000)
-	.url('https://cedev.channelauction.com/#/app/cas/pricing-package-edit/9b3a6a13-455f-4d54-98e0-dcd9c51e2e1a') //'https://cedev.channelauction.com/#/app/cas/pricing-package-setup'
-	//.waitForElementVisible('table[data-elm-id="tblPricePackageListing"]', 80000)
-	//.click('a[class="ng-binding" value="Tex-X PP"]')
-	.waitForElementVisible('select[id="SelectedStates"]', 80000)
-	*/
 	browser
-	// Generate a valid address
-	/*
-	.url('http://www.fakeaddressgenerator.com/')
-	.useXpath()
-	.waitForElementVisible('html/body/div[1]/div[3]/div[2]/div[1]/form/div[4]/div[2]/input', 5000)	
-	.setValue('html/body/div[1]/div[3]/div[2]/div[1]/form/div[4]/div[2]/input', zipCode)
-	.click('html/body/div[1]/div[3]/div[2]/div[1]/form/div[6]/input')
-	.waitForElementVisible('html/body/div[1]/div[3]/div[1]/div/div[3]/div[2]/div[6]/div[2]/strong/input', 5000)
-	.getValue('html/body/div[1]/div[3]/div[1]/div/div[3]/div[2]/div[6]/div[2]/strong/input', function(result){ line1 = result.value; console.log('Address: '+line1) })
-	*/
 	.perform(
 		function() {
 			browser.url('https://dedev.channelauction.com/homes/login?redirectUrl=%2Fhomes%2F')
@@ -43,103 +22,80 @@ module.exports = {
 			browser.url('https://dedev.channelauction.com/homes/list-a-home/search')
 			browser.waitForElementVisible('input[ng-model="locatePropCtrl.model.address"]', 80000)
 			browser.setValue('input[ng-model="locatePropCtrl.model.address"]', address)
-			browser.waitForElementVisible('ul[class="dropdown-menu ng-isolate-scope"]', 80000)
-			browser.keys.ENTER //browser.keys(['\uE006'])
+			browser.pause(1000)
+			browser.keys.ENTER								
 			browser.click('button[btn-whirl="btn-whirl"]')
 
-				// Verify if modal window pops up after the realty tracking
+			// Verify if modal window shows up after the realty tracking
 				if (browser.waitForElementVisible('div[class="modal-content"]', 80000)){
 					browser.click('button[btn-whirl="btn-whirl"]')
-				}
+				}	
 
-			browser.waitForElementVisible('div[class="col-md-3 hidden-xs hidden-sm"] li[class="ng-scope active"] a[class="ng-binding ng-scope"]', 8000)
-			browser.getText('div[class="col-md-3 hidden-xs hidden-sm"] li[class="ng-scope active"] a[class="ng-binding ng-scope"]', function(result){ currentNavOption = result.value; console.log(currentNavOption === "Listing Terms *"); })
+			browser.waitForElementVisible('body', 80000)
 
-			// THE SELLER JOURNEY PROCESS GETS STARTED:
+		// THE SELLER JOURNEY GETS STARTED:		
+		// Complete step in the SJ: Listing Package
+			browser.waitForElementVisible('a[ng-click="listingPackageCtrl.getStarted()"]', 80000)
+			browser.click('a[ng-click="listingPackageCtrl.getStarted()"]')
 
-				// Verify if the current step in the SJ is Listing Package
-				if (currentNavOption === 'Listing Package ') {
-					browser.click('a[ng-click="listingPackageCtrl.getStarted()"]')
-				}
+		// Complete step in the SJ: Seller Info
+			browser.waitForElementVisible('button[ng-click="declCtrl.setUserIsAgent(false)"]', 80000)
+			browser.click('button[ng-click="declCtrl.setUserIsAgent(false)"]')									
+			browser.waitForElementVisible('button[ng-click="declCtrl.setOwnerHasAgent(false)"]', 80000)
+			browser.click('button[ng-click="declCtrl.setOwnerHasAgent(false)"]')
+			browser.click('button[data-elm-id="btnSave"]')
 
-				// Verify if the current step in the SJ is Seller Info
-				if (currentNavOption === 'Seller Info *') {
-					browser.click('button[ng-click="declCtrl.setUserIsAgent(false)"]')
-					//browser.pause(1000)
-					//browser.waitForElementVisible('button[ng-click="declCtrl.setOwnerHasAgent(false)"]', 5000)
-					browser.click('button[ng-click="declCtrl.setOwnerHasAgent(false)"]')
-					browser.click('button[data-elm-id="btnSave"]')
-				}
+		// Complete step in the SJ: Property Info
+			browser.waitForElementVisible('select[data-elm-id="propertyType"] option[label="Single Family-Attached"]', 80000)
+			browser.click('select[data-elm-id="propertyType"] option[label="Single Family-Attached"]')
+			browser.waitForElementVisible('select[data-elm-id="beds"] option[label="2"]', 80000)			
+			browser.click('select[data-elm-id="beds"] option[label="2"]')	
+			browser.waitForElementVisible('select[data-elm-id="completeBathrooms"] option[label="3"]', 80000)								
+			browser.click('select[data-elm-id="completeBathrooms"] option[label="3"]')
+			browser.setValue('input[data-elm-id="squareFootage"]', '3025')
+			browser.setValue('input[data-elm-id="lotSizeSqft"]', '1765')
+			browser.setValue('input[id="yearBuilt"]', '1999')
+			browser.click('button[data-elm-id="btnSave"]')
+			browser.pause(2000)
 
-				// Verify if the current step in the SJ is Property Info
-				if (currentNavOption === 'Property Info *') {
-					browser.click('select[data-elm-id="propertyType"] option[label="Single Family-Attached"]')
-					//browser.pause(500)
-					browser.click('select[data-elm-id="beds"] option[label="2"]')
-					//browser.pause(500)
-					browser.click('select[data-elm-id="completeBathrooms"] option[label="3"]')
-					browser.setValue('input[data-elm-id="squareFootage"]', '3025')
-					browser.setValue('input[data-elm-id="lotSizeSqft"]', '1765')
-					browser.setValue('input[id="yearBuilt"]', '1999')
-					browser.click('button[data-elm-id="btnSave"]')
-				}
+		// Complete step in the SJ: Amenities							
+			browser.click('button[data-elm-id="btnSave"]')
+			browser.pause(2000)		
 
-				// Verify if the current step in the SJ is Amenities
-				if (currentNavOption === 'Amenities ') {
-					browser.click('input[data-elm-id="hasCentralAirConditioning"]')
-					browser.click('button[data-elm-id="btnSave"]')		
-				}
+		// Complete step in the SJ: Description
+			browser.click('button[data-elm-id="btnSave"]')
+			browser.pause(2000)
 
-				// Verify if the current step in the SJ is Description
-				if (currentNavOption === 'Description ') {
-					browser.click('button[data-elm-id="btnSave"]')
-				}
+		// Complete step in the SJ: Listing Terms							
+			browser.waitForElementVisible('input[data-elm-id="listPrice"]', 80000)
+			browser.setValue('input[data-elm-id="listPrice"]', '350000')
+			browser.click('button[data-elm-id="btnSave"]')			
 
-				// Verify if the current step in the SJ is Listing Terms
-				if (currentNavOption === "Listing Terms *") {
-					browser.pause(800)
-					browser.setValue('input[data-elm-id="listPrice"]', '350000')
-				}
+		// Complete step in the SJ: Photos
+			browser.waitForElementVisible('div[uploader="lpCtrl.page.uploader"]', 80000)
+			browser.setValue('input[type="file"]', require('path').resolve('./'+photo))
+			browser.pause(1000)
+			browser.click('button[data-elm-id="btnAssignPhotos"]')																																							
+			browser.waitForElementVisible('div[class="img-content"]', 80000)
+			browser.click('div[class="img-content"]')
+			browser.pause(1000)
+			browser.click('button[data-elm-id="btnTakeAction"]')
+			browser.pause(1000)
+			browser.click('button[data-elm-id="btnSaveAndPreview"]')
 
-				// Verify if the current step in the SJ is Photos
-				if (currentNavOption === 'Photos *') {
-					
-				}
+		// List the home just created
+			browser.waitForElementVisible('button[data-elm-id="btnActivateListingXs"]', 80000)		
+			browser.click('button[data-elm-id="btnActivateListingXs"]')	
 
-
-/*
-			if (currentNavOption = 'Listing Package ') {
-				//browser.setValue('input[data-elm-id="listPrice"]', '350000')
-			} else if (currentNavOption = 'Seller Info ') {
-
-			}
-			
-*/			
+		// Activate Listing - Marketing Agreement
+			browser.waitForElementVisible('div[class="marketing-signature ng-binding"]', 80000)
+			browser.waitForElementVisible('button[data-elm-id="btnAgreeContinue"]', 80000)
+			browser.click('button[data-elm-id="btnAgreeContinue"]')	
 		
-
-
-
-
-
-
-/*
-			if (browser.waitForElementVisible('a[ng-click="listingPackageCtrl.getStarted()"]', 5000)){
-				browser.click('a[ng-click="listingPackageCtrl.getStarted()"]')
-				browser.waitForElementVisible('body', 5000)	
-			}
-
-			if (browser.waitForElementVisible('button[ng-click="declCtrl.setUserIsAgent(false)"]', 5000)){
-				browser.click('button[ng-click="declCtrl.setUserIsAgent(false)"]')
-			}
-			
-	*/
-			//
-			//
-			//browser.waitForElementVisible('button[ng-click="declCtrl.setOwnerHasAgent(false)"]', 80000)
-			//browser.click('button[ng-click="declCtrl.setOwnerHasAgent(false)"]')
-			//browser.click('button[data-elm-id="btnSave"]')
-			}
-		)
-	
-	}
+		// Verify on DE if property was created and listed successfully
+			browser.waitForElementVisible('div[ng-if="activityDashboard.homesListedData.length > 0"]', 80000)
+			browser.assert.containsText('div[ng-if="activityDashboard.homesListedData.length > 0"]', address)
+		}
+	)
+  }
 };
